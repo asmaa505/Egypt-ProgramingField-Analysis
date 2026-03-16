6ca5bbca5d0bb6bfecfc1b4689a5cd4c83e8dbc4
asmaa505 <asmaaahmedt52@gmail.com>
2026-03-17 00:27:43 +0200

    filter not egyption data from dataset 3

--- Dataset/design.py
+++ Dataset/design.py
@@ -3,20 +3,40 @@
 # pip install openpyxl
 
 
+# import
 import pandas as pd
 import streamlit as st
 
-df = pd.read_csv('Web-Developers-Salaries-Egypt-2024.csv' )
-st.write( df )
+
+#! dataset 1
+df1 = pd.read_csv('Web-Developers-Salaries-Egypt-2024.csv' )
+st.write( df1 )
 
 
+
+#! dataset 2
 df2 = pd.read_csv('Cleaned_Wuzzuf_Jobs.csv')
 st.write( df2 )
 
 
+
+#! dataset 3
 df3 = pd.read_excel( 'dataset3.xlsx' )
 st.write( df3 )
 
 
-df4 = df[df['What Is your Company'] != 'Not Egyptian and site out of egypt']
-st.write(df)
+
+#! dataset 4
+keywords = [
+    'not egyptian', 
+    'site out of egypt', 
+    'foreign', 
+    'non egypt', 
+    'not egypt'
+]
+
+mask = df3.apply(lambda row: row.astype(str).str.lower().str.contains('|'.join(keywords)).any(), axis=1)
+
+df3_clean = df3[~mask]
+
+st.write(df3_clean)
